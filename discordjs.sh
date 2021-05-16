6dd7624f8bb6ca2f21cf5f662fdda2cb01010187
mikespeelg <53977964+mikespeelg@users.noreply.github.com>
2021-05-16 10:49:36 +0200

    Create discordjs.sh

---
+++ discordjs.sh
@@ -0,0 +1,3 @@
+echo Welcome to galaxichost.com discord.js hosting
+if [[ -d .git ]] && [[ {{AUTO_UPDATE}} == "1" ]]; then git pull; fi; if [[ ! -z ${NODE_PACKAGES} ]]; then /usr/local/bin/npm install ${NODE_PACKAGES}; fi; if [ -f /home/container/package.json ]; then  /usr/local/bin/npm install --production; fi; /usr/local/bin/node /home/container/{{BOT_JS_FILE}}
+echo Server started
