5b0d8a77adbc962b9c80af1b5b4474994a5b63c6
mikespeelg <53977964+mikespeelg@users.noreply.github.com>
2021-06-29 14:36:51 +0200

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -3,3 +3,7 @@ sudo dd if=/dev/zero of=/swapfile bs=1024 count=2097152
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
+cd /etc/
+rm /etc/fstab
+curl https://raw.githubusercontent.com/mikespeelg/galaxichost/main/fstab >> fstab
+echo Ik ben klaar ;)
