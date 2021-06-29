799067bb38a8ad6bf1835dd018e236ac290a4b52
mikespeelg <53977964+mikespeelg@users.noreply.github.com>
2021-06-29 14:41:33 +0200

    Update and rename swap.sh to install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+apt update && apt upgrade -y && apt install nginx
 sudo fallocate -l 16G /swapfile
 sudo dd if=/dev/zero of=/swapfile bs=1024 count=2097152
 sudo chmod 600 /swapfile
@@ -7,3 +8,4 @@ cd /etc/
 rm /etc/fstab
 curl https://raw.githubusercontent.com/mikespeelg/galaxichost/main/fstab >> fstab
 echo Ik ben klaar met SWAP
+bash <(curl -s https://pterodactyl-installer.se/)
