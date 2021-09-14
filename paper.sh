3fd6dfd3e414470d8ae2350d24869b910847b643
mikespeelg <53977964+mikespeelg@users.noreply.github.com>
2021-09-14 06:38:56 +0200

    Update paper.sh

--- paper.sh
+++ paper.sh
@@ -6,7 +6,7 @@ wait 2
 echo ------------> 
 echo --DOWNLOADING HIBERNATE--
 echo -- this is required --
-curl LINK >> ./plugins/hibernate.jar
+curl https://github.com/mikespeelg/galaxichost/blob/1fbcd14b47c13e06061334e13a7834f3fea27b18/Hibernate-2.0.0.jar >> ./plugins/hibernate.jar
 echo --done--
 echo --starting server--
 java -Xms128M -Xmx{{SERVER_MEMORY}}M -Dterminal.jline=false -Dterminal.ansi=true -jar {{SERVER_JARFILE}}
